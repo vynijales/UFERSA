d7d6414d398f950e2d8de85ab3ff350838ebfef3
vynijales <vynijales@gmail.com>
2023-02-26 15:00:19 -0300

    Lab03 #1

---
+++ C++/Labs/Lab03/questao1.cpp
@@ -0,0 +1,16 @@
+// Utilize apenas uma instrução cin para ler as três informações abaixo para variáveis 
+// diferentes. Multiplique o valor das variáveis altura, largura e comprimento para 
+// achar o volume do cubo.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Insira as dimensoes da figura geometrica (altura, largura e altura): ";
+    cin >> a >> b >> c;
+    cout << "Volume: " << a * b * c;
+}
+
