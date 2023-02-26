30d8b6c364fcaaf8d4a125ca7dc4ebfde0249a3c
vynijales <vynijales@gmail.com>
2023-02-26 16:41:33 -0300

    Lab03 #2

---
+++ C++/Labs/Lab03/revisaoQuestao2.cpp
@@ -0,0 +1,15 @@
+// Utilize instruções cin para ler o valor de horas e minutos para variáveis diferentes.
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int hora, min;
+    char c;
+
+    cout << "Que horas sao? ";
+    cin >> hora >> c >> min;
+    cout << "Sao " << setfill('0') << setw(2) << hora << c << setfill('0') << setw(2) << min;
+    // Adiciona um zero a esquerda quando necessario, para preencher o "width" (largura) definido em setw().
+}
