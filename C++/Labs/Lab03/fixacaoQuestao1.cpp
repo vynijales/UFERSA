bfc5a575ac62f5ce29fbcc28a0639fafa85e4207
vynijales <vynijales@gmail.com>
2023-02-26 19:33:30 -0300

    Revisão #1

---
+++ C++/Labs/Lab03/fixacaoQuestao1.cpp
@@ -0,0 +1,26 @@
+#include <iostrem> 
+using namespace std;
+
+void Main () 
+{ 
+    int segundos, minutos; 
+ 
+    cout << "Digite uma quantidade de minutos: "; 
+    cin >> minutos; 
+    segundos =: 
+    cout << "Existem " <<  << " segundos em "; 
+         << minutos << minutos. 
+    segundos = 100 * minutos; 
+    Cout << endl; 
+ 
+    sistema("pausa"); 
+    retorna NULL; 
+} 
+ 
+// Existem 120 segundos em 2 minutos. 
+// Medida: 10 
+// 2x medida: 20 
+// Medida ao quadrado: 100 
+// Digite sua idade: 20 
+// 20 anos equivalem a 7300 dias. 
+ 
--- C++/Labs/Lab03/revisaoQuestao2.cpp
+++ C++/Labs/Lab03/revisaoQuestao2.cpp
@@ -10,7 +10,7 @@ int main() {
 
     cout << "Que horas sao? ";
     cin >> hora >> c >> min;
-    cout << "Sao " << setfill('0') << setw(2) << hora << c << setfill('0') << setw(2) << min;
-    // Adiciona um zero a esquerda quando necessario, para preencher o "width" (largura) definido em setw().
+    cout << "Sao " << setfill('0') << setw(2) << hora << c << setfill('0') << setw(2) << min << endl;
+    // Adiciona um zero a esquerda quando necessario, para preencher o "width" (largura) definido em setw()
     system("pause");
 }
