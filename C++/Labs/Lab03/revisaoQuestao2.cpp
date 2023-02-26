6ff7c6d8de1b6302e040ccd43f3030fdbb65a301
vynijales <vynijales@gmail.com>
2023-02-26 19:34:39 -0300

    Revisão #2

--- C++/Labs/Lab03/revisaoQuestao2.cpp
+++ C++/Labs/Lab03/revisaoQuestao2.cpp
@@ -11,6 +11,6 @@ int main() {
     cout << "Que horas sao? ";
     cin >> hora >> c >> min;
     cout << "Sao " << setfill('0') << setw(2) << hora << c << setfill('0') << setw(2) << min << endl;
-    // Adiciona um zero a esquerda quando necessario, para preencher o "width" (largura) definido em setw()
+    // Adiciona um zero a esquerda quando necessario, para preencher o "width" (largura) definido em setw().
     system("pause");
 }
