0eb397f08f3651e9928ecfdb9a0ebe12a33da627
Matheus Vynicius <92933508+vynijales@users.noreply.github.com>
2023-02-28 16:39:04 -0300

    Revisão #2

---
+++ C++/Labs/Lab04/revisaoQuestao02.cpp
@@ -0,0 +1,28 @@
+// Uma forma de obter um número pseudoaleatório mais imprevisível é usar um
+// valor de semente que mude com o tempo. A biblioteca <ctime> possui uma função
+// time() que retorna uma quantidade de segundos transcorrida (geralmente desde
+// 1 de janeiro de 1970) quando passamos NULL como argumento da função.
+// // srand(time(NULL));
+// Por fim, exiba a mensagem GRANDE se o número obtido for maior que 16834 e
+// PEQUENO caso contrário. 
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    
+    int n;
+    srand(time(NULL));
+    n = rand() % 32000;
+    
+    cout << "Gerando numeros pseudoaleatorios:" << endl;
+    if (n < 16834) {
+        cout << n << " PEQUENO" << endl;
+    } else {
+        cout << n << " GRANDE" << endl;
+    }
+
+    system("pause");
+}
