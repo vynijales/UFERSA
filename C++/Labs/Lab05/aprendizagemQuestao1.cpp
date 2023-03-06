c327f55fe369308e62f51fb13358c8bf5964ab32
vynijales <vynijales@gmail.com>
2023-03-06 13:25:04 -0300

    Aprendizagem #1

---
+++ C++/Labs/Lab05/aprendizagemQuestao1.cpp
@@ -0,0 +1,10 @@
+// Qual é a diferença entre as instruções de inicialização a seguir? Existe alguma 
+// razão para preferir uma opção sobre a outra? O resultado delas depende de onde 
+// elas estão dentro do código?
+/////////////
+// int x; 
+// x = 1;
+/////////////
+// int y = 1;
+
+// Resposta: Da primeira forma, é declarado a variável e, posteriormente é definido um valor para ela. Já a segunda, a variável é declarada ao mesmo tempo que é definido um valor.
